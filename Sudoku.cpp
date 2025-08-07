4b5fa16ba4363f6641b9dde45e139c3c6098bad0
Prathwik <75462688+prathwiknaik08@users.noreply.github.com>
2025-08-07 16:45:21 +0530

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,15 +1,4 @@
-﻿/**
-	Dancing links Sudoku solver - Richard Haar 2022
 
-	Solves a sudoku by representing the problem as an exact cover problem
-	and solves it using Donald Knuth's AlgorithmX with dancing links
-	for efficient backtracking.
-
-	See Donald Knuth's paper here: https://arxiv.org/pdf/cs/0011047.pdf
-
-	Zendoku also has a good article explaining the dancing links algorithm:
-	https://garethrees.org/2007/06/10/zendoku-generation/
-*/
 #include "Sudoku.h"
 
 #include <cstdint>
